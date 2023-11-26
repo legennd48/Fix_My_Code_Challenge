794a12439a5a3356513de88a93f3e3022a3eee8a
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-11-26 08:17:17 +0000

    fixed typo

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -55,7 +55,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().lower()  == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
         #removed .upper() and replaced with .lower
 
 
