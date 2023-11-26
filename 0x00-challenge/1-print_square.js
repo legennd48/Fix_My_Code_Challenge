461750ecc3fccee4653e6c3d88e3bf78f988d495
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-11-26 07:15:57 +0000

    removed previous and converted argument to int

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,12 +14,13 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = process.argv[2]
+size = parseInt(process.argv[2]);
 
+/*
 if (size <= 0) {
     process.stdout.write("\n");
 }
-
+*/
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
